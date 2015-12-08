44504abda1908c685b130592337b1015dd52ff08
Oyorzaba <oyorzabamichel1@hotmail.com>
2015-12-08 02:26:07 +0100

    Tareas

---
+++ Matrizes.c.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+    int identidad[3][3], i, j, esc;
+    int main()
+	{
+	printf("Su matriz identidad es\n");
+	identidad[0][0]=1;
+	identidad[0][1]=0;
+	identidad[0][2]=0;
+	identidad[1][0]=0;
+	identidad[1][1]=1;
+	identidad[1][2]=0;
+	identidad[2][0]=0;
+	identidad[2][1]=0;
+	identidad[2][2]=1;
+	for(i=0; i<3; i++){
+		for(j=0; j<3; j++){
+			printf("%d", identidad[i][j]);
+		}printf("\n");
+	}printf("Da el valor del escalar: \n");
+	scanf("%d", &esc);
+	for(i=0; i<3; i++){
+		for(j=0; j<3; j++){
+			identidad[i][j]=identidad[i][j]*esc;
+			printf("%d", identidad[i][j]);
+		}printf("\n");
+	}
+	}
